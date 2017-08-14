6cf980b663b03429c675c39406403de874fca680
renukumars <30857138+renukumars@users.noreply.github.com>
2017-08-14 19:28:12 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,6 +9,16 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+app.get('/Article-one', function (req, res)
+{res.send('Article one will be served here'); 
+});
+
+
+
+
+
+
+
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
