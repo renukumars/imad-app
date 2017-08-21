88f6020dc788e07957eb9c789b19f76f842d28c7
renukumars <30857138+renukumars@users.noreply.github.com>
2017-08-21 13:29:30 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -85,8 +85,7 @@ app.get('/', function (req, res) {
 });
 
 app.get('/:articleName', function (req, res){
-    var articleName = req.params.articleName;
-    res.send(createTemplate(articles[articleName]));
+    res.sendFile(path.join(__dirname, 'ui', 'artical-one.html'));
 });
 
 app.get('/ui/style.css', function (req, res) {
