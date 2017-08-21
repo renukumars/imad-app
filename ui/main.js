7baef1d94713c9b101c16d4dd1bbfec3fc617ed5
renukumars <30857138+renukumars@users.noreply.github.com>
2017-08-21 19:19:12 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,6 +3,6 @@ var counter=0;
 button.onclick = function(){
     counter = counter +1;
     var span = document.getElementById('count');
-    span.innerhtml = counter.toString();
+    span.innerHTML = counter.toString();
     
 };
